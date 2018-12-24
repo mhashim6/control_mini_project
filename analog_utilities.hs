92d481c20aa8362571947b7057d6ec424c601f99
mhashim6 <m6hashim@gmail.com>
2018-12-24 20:01:37 +0200

    new functions to calculate the amplified error voltage. fixed bugs in some functions.

--- analog_utilities.hs
+++ analog_utilities.hs
@@ -1,6 +1,6 @@
 -- converts a sampled analog signal to actual voltage value with respect to VCC.
 atv ::  Float -> Float -> Float
-atv vcc a  = (a / 255.0) * vcc
+atv vcc a  = (a / 1024.0) * vcc
 
 -- converts voltage to a sampled analog signal  with respect to VCC.
 vta ::  Float -> Float -> Float
@@ -11,6 +11,13 @@ atc ::  Float -> Float
 atc a = a * 0.48828125
 -- same as: atc a = 100.0 * atv a 5.0
 
+-- converts an amplified sampled (LM35) analog signal to Celsius (assuming 5v VCC).
+agtc :: Float -> Float
+agtc ag = ag * 0.09765625
+
+-- does the opposite of agtc.
+ctag  :: Float -> Float
+ctag c = c / 0.09765625
 -- does the opposite of atc.
 cta ::  Float -> Float
 cta c = c / 0.48828125
